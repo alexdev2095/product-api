fa93356fd24f1e3eb25f1bf18dde98d64da08e9a
justo <justo.medina@uic.cu>
2025-06-02 21:08:10 -0400

    prisma revisado

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,73 +1,9 @@
-// import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-// import { PrismaClient } from '@prisma/client';
-
-// @Injectable()
-// export class PrismaService extends PrismaClient implements OnModuleInit {
-//   async onModuleInit() {
-//     await this.$connect();
-//   }
-
-//   async enableShutdownHooks(app: INestApplication) {
-//     this.$on('beforeExit', async () => {
-//       await app.close();
-//     });
-//   }
-// }
-
-// import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-// import { PrismaClient } from '@prisma/client';
-
-// @Injectable()
-// export class PrismaService extends PrismaClient implements OnModuleInit {
-//   constructor() {
-//     super({
-//       log: ['query', 'info', 'warn', 'error'],
-//     });
-//   }
-
-//   async onModuleInit() {
-//     await this.$connect();
-//   }
-
-//   async enableShutdownHooks(app: INestApplication) {
-//     this.$on('beforeExit', async () => {
-//       await app.close();
-//     });
-//   }
-// }
-
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  private static instance: PrismaService;
-
-  constructor() {
-    super({
-      log: ['warn', 'error'],
-    });
-
-    if (!PrismaService.instance) {
-      PrismaService.instance = this;
-    }
-
-    return PrismaService.instance;
-  }
-
   async onModuleInit() {
-    try {
-      await this.$connect();
-      console.log('Prisma connected successfully');
-    } catch (error) {
-      console.error('Prisma connection error', error);
-      process.exit(1);
-    }
-  }
-
-  async enableShutdownHooks(app: INestApplication) {
-    this.$on('beforeExit', async () => {
-      await app.close();
-    });
+    await this.$connect();
   }
 }
